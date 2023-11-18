612a05bf7b028a22b80edc7934146a1d9355b9aa
Miller Alvarez <mdalvarezb@ufpso.edu.co>
2023-11-18 16:09:56 -0500

    Primer Commit

---
+++ server.js
@@ -0,0 +1,52 @@
+const express = require('express');
+const app = express();
+const http = require('http');
+const server = http.createServer(app);
+const logger = require('morgan');
+const cors = require('cors');
+const mongoBd = require('./config/config')
+
+const users = require('./routes/routes');
+
+const port = process.env.PORT || 3000;
+
+app.use(logger('dev'));
+app.use(express.json());
+
+mongoBd();
+
+app.use(express.urlencoded({
+    extended: true
+}));
+
+app.use(cors());
+
+app.disable('x-powered-by');
+
+app.set('port', port);
+
+server.listen(3000, '192.168.1.5' || 'localhost', function () {
+    console.log('Aplicacion NodeJS id=>' + process.pid + ' puerto=>' + port + ' iniciando...')
+});
+
+/*app.get('/', (req, res) => {
+    res.send('Ruta raiz del BackEnd');
+});
+
+app.get('/una', (req, res) => {
+    res.send('Ruta una del backend');
+});*/
+
+// Manejo de errores
+
+app.use((err, req, res, next) => {
+    console.log(err);
+    res.status(err.status || 500).send(err.stack);
+});
+
+
+users(app);
+module.exports = {
+    app: app,
+    server: server
+};
